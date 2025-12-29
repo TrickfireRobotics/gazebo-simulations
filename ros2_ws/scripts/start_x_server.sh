c332aa7222c0552011263775c18a8a76bc73c4c6
Matej Stastny <matysta@outlook.com>
2025-12-29 06:26:33 +0000

    Added proper documentation to the x server script

--- ros2_ws/scripts/start_x_server.sh
+++ ros2_ws/scripts/start_x_server.sh
@@ -1,13 +1,64 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Xvfb "$DISPLAY" -screen 0 "${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH}" -ac +extension GLX +render -noreset &
-# XVFB_PID=$!
+# --------------------------------------------------------------------------------------------
+# start_x_server.sh
+# --------------------------------------------------------------------------------------------
+# Starts a complete headless X11 desktop environment inside the container.
+#
+# This script sets up:
+#   - A virtual X server (Xorg) using a dummy display
+#   - A lightweight window manager (Openbox)
+#   - A VNC server (x11vnc) for remote access
+#
+# Intended for:
+#   - Running GUI apps (Gazebo) in a Docker container
+# --------------------------------------------------------------------------------------------
 
-Xorg :1 -noreset -config /etc/X11/xorg.conf &
+set -e
+
+# --------------------------------------------------------------------------------------------
+# START X11 SERVER (HEADLESS)
+# --------------------------------------------------------------------------------------------
+# Launch an Xorg server on display :1
+#
+# Flags:
+#   $DISPLAY              -> Target display (defined in Dockerfile)
+#   -noreset              -> Prevent Xorg from restarting when clients disconnect
+#   -config xorg.conf     -> Use a custom dummy display configuration
+#
+Xorg "$DISPLAY" -noreset -config /etc/X11/xorg.conf &
 sleep 1
 
-openbox-session &
-WM_PID=$!
 
-x11vnc -display "$DISPLAY" -forever -shared -rfbport "$VNC_PORT" -nopw -xkb &
-VNC_PID=$!
+# --------------------------------------------------------------------------------------------
+# START WINDOW MANAGER
+# --------------------------------------------------------------------------------------------
+# Openbox is a very lightweight window manager.
+# It provides:
+#   - Window decorations
+#   - Basic window movement / resizing
+#   - A usable desktop environment
+#
+openbox-session &
+
+
+# --------------------------------------------------------------------------------------------
+# START VNC SERVER
+# --------------------------------------------------------------------------------------------
+# x11vnc allows remote access to the X11 display over VNC.
+#
+# Flags:
+#   -display "$DISPLAY"   -> Display used in Xorg (defined in Dockerfile)
+#   -forever              -> Keep running after clients disconnect
+#   -shared               -> Allow multiple VNC clients at once
+#   -rfbport "$VNC_PORT"  -> Port to listen on (defined in Dockerfile)
+#   -nopw                 -> Disable password auth
+#   -xkb                  -> Enable proper keyboard mapping
+#
+x11vnc \
+    -display "$DISPLAY" \
+    -forever \
+    -shared \
+    -rfbport "$VNC_PORT" \
+    -nopw \
+    -xkb &
