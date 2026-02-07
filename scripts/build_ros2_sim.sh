d8b285c2045fcd6bced10c86b87e66603fe02e52
Matej Stastny <matysta@outlook.com>
2026-02-07 03:31:19 +0000

    Improved comments in ROS build script

--- scripts/build_ros2_sim.sh
+++ scripts/build_ros2_sim.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # --------------------------------------------------------------------------------------------
-# build-ros2-sim.sh
+# build_ros2_sim.sh
 # --------------------------------------------------------------------------------------------
 # Builds the ROS 2 Gazebo simulation workspace, sets up the environment,
 # registers custom Gazebo models, and launches the ROS2 robot simulation.
 # --------------------------------------------------------------------------------------------
 
-# Checkout Sim template folder
 cd /home/trickfire/gazebo-simulations/robot-sim || exit 1
 
-# Build all of the folders Within our Folder
+# Build our robot files using the ROS build tool
 colcon build --cmake-args -DBUILD_TESTING=ON
 
-# Give setup script executable perms
+# Give generated ROS setup script executable perms
 chmod a+x install/setup.bash
 
 # Source our newly build dependencies
