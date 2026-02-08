0cb051d9f5e6f8ecc3b2c1a2235d3b0b11d366e3
Calin Prutean <calinprutean@Calins-MacBook-Pro.local>
2026-02-08 00:35:09 -0800

    Added logging functionality to build script for easier debugging

--- scripts/build_ros2_sim.sh
+++ scripts/build_ros2_sim.sh
@@ -6,6 +6,10 @@
 # Builds the ROS 2 Gazebo simulation workspace, sets up the environment,
 # registers custom Gazebo models, and launches the ROS2 robot simulation.
 # --------------------------------------------------------------------------------------------
+cd /home/trickfire/gazebo-simulations/scripts/log/
+logFile="log-$(date +'%Y-%m-%d_%H-%M').log"
+
+exec > >(tee -a "$logFile") 2>&1
 
 cd /home/trickfire/gazebo-simulations/robot-sim || exit 1
 
