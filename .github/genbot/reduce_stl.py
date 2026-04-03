73253fb9c06b925001caf108782780beca4d6a20
Calin Prutean <calinprutean@Calins-MacBook-Pro.local>
2026-04-03 14:30:01 -0700

    Updated logging in reduce_stl to include better logging

--- .github/genbot/reduce_stl.py
+++ .github/genbot/reduce_stl.py
@@ -14,7 +14,7 @@ import log
 MIN_TRIANGLES = 200
 
 
-def reduce_file_size(input_file_path: str, output_file_path: str, target_triangles) -> list[float]:
+def reduce_file_size(input_file_path: str, output_file_path: str, target_triangles) -> list[int]:
     """Reduces the triangle count of a STL file passed to the method"""
     og_fsize = os.path.getsize(input_file_path)
 
@@ -25,7 +25,7 @@ def reduce_file_size(input_file_path: str, output_file_path: str, target_triangl
     if og_len <= target_triangles:
         print(f"Mesh {input_file_path} is already below target. Saving as is.\n")
         o3d.io.write_triangle_mesh(output_file_path, mesh, write_ascii=False)
-        return
+        return None
     decimated_mesh = mesh.simplify_quadric_decimation(target_number_of_triangles=target_triangles)
     decimated_mesh.compute_vertex_normals()
     o3d.io.write_triangle_mesh(output_file_path, decimated_mesh, write_ascii=False)
@@ -46,8 +46,8 @@ def batch_process_directory(input_dir, output_dir, reduction_ratio=0.4):
     # Find all STL files in the input directory
     stl_files = glob.glob(os.path.join(input_dir, "*.stl"))
     part_files = glob.glob(os.path.join(input_dir, "*.part"))
-    old_file_size = 0.0
-    new_file_size = 0.0
+    old_file_size = 0
+    new_file_size = 0
 
     for file_path in stl_files:
         filename = os.path.basename(file_path)
@@ -60,9 +60,13 @@ def batch_process_directory(input_dir, output_dir, reduction_ratio=0.4):
         target = int(current_triangles * reduction_ratio)
 
         r = reduce_file_size(file_path, out_path, target)
-        old_file_size += r[0]
-        new_file_size += r[1]
-    log.info(f"reduced folder size from {old_file_size} to {new_file_size}")
+        if r is not None:
+            old_file_size += r[0]
+            new_file_size += r[1]
+    old_file_size = old_file_size / 1000
+    new_file_size = new_file_size / 1000
+    log.info(f"reduced folder size from {old_file_size} kb to {new_file_size} kb")
+
     # copy the rest of the .part files
     for file_path in part_files:
         filename = os.path.basename(file_path)
