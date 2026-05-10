efcb07e2694def201969221f7e08e7b7bb7711bf
CPrutean <calin.prutean@gmail.com>
2026-05-09 19:06:45 -0700

    Fixed native sim launch on mac

--- cli/native/shell/macos_workspace.sh
+++ cli/native/shell/macos_workspace.sh
@@ -10,12 +10,18 @@ ROS_GZ_WS="$4"
 CONTROL_WS="$5"
 WORKSPACE_DIR="$6"
 
+#this looks weird but leaving some environment variables unset is ok
+#they set themselves later in the sourcing
+set +u
+
 eval "$($MAMBA_EXE shell hook --shell bash)"
 micromamba activate "$ENV_PREFIX"
 
+# ROS setup.bash files reference unset path vars; seed them so `set -u` doesn't trip.
 source "$ROS_BASE/setup.bash"
 source "$ROS_GZ_WS/install/setup.bash"
 source "$CONTROL_WS/install/setup.bash"
+set +u
 
 LINKER_FLAGS="-Wl,-headerpad_max_install_names -Wl,-rpath,$ENV_PREFIX/lib -Wl,-rpath,$ROS_BASE/lib -Wl,-rpath,$ROS_GZ_WS/install/lib -Wl,-rpath,$CONTROL_WS/install/lib -L$ENV_PREFIX/lib -L$ROS_BASE/lib -undefined dynamic_lookup"
 
