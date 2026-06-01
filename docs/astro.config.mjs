6c3af23a5d7378cfdb28cf8963362ca57aec5b9e
Matej Stastny <matysta@outlook.com>
2026-06-01 13:18:55 -0700

    fix: Pages assets path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,7 +3,6 @@ import starlight from '@astrojs/starlight';
 
 export default defineConfig({
     site: 'https://docs.trickfirerobotics.com',
-    base: '/gazebo-simulations',
     srcDir: './',
     integrations: [
         starlight({
